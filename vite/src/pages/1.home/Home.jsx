ee6ccd4c35c3ec9949a65a5440d550b85eb390b4
sachal1 <71674805+sachal1@users.noreply.github.com>
2023-11-07 19:53:08 -0800

    initial commit

---
+++ vite/src/pages/1.home/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+      <div>
+          <h1 className="text-center p-4">HOMEPAGE</h1>
+          <p className="mx-5"> Welcome to home</p>
+      </div>
+    )
+  }
+  
+  export default Home
