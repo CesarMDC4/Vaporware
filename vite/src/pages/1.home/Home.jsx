cd1da97ee9409d7e981c13fe961ee8ee48526cda
ElesiaThan <aiselenaht@gmail.com>
2023-11-09 20:34:39 -0800

    Added intro and register button

--- vite/src/pages/1.home/Home.jsx
+++ vite/src/pages/1.home/Home.jsx
@@ -1,9 +1,14 @@
 
 const Home = () => {
     return (
-      <div>
-          <h1 className="text-center p-4">HOMEPAGE</h1>
-          <p className="mx-5"> Welcome to home</p>
+      /*Intro Section */
+      <div className="text-center p-4">   
+        <p class="text-red-500 font-bold text-xl pb-5">Upcoming training opportunities!</p>
+        <p class="font-bold pb-5">Oct & Nov 2023 in Monterey, CA</p>
+        <p class="pb-5">Ammonia Refrigeration, Regulatory Compliance, and HAZWOPER Training</p>
+        <p class="pb-5">In-person and online options available.</p>
+        <p class="pb-5">Click below to go to our new training registration page for more info and to register.</p>
+        <button class="bg-green-900 text-white hover:bg-white hover:text-green-900 font-bold py-2 px-4 rounded-full">Register Now!</button>
       </div>
     )
   }
