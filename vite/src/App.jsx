8d030512c69ca90797de43cfc005cd430a906492
sachal1 <71674805+sachal1@users.noreply.github.com>
2023-11-11 18:38:44 -0800

    Story VAP - 28

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -3,9 +3,19 @@ import './App.css'
 import Navbar from "./components/header/Navbar";
 import Home from "./pages/1.home/Home";
 import Services from './pages/2.services/Services';
-import Portfolio from './pages/3.portfolio/Portfolio';
 import Contact from './pages/4.contact/Contact';
 import Footer from './components/footer/Footer';
+import Training from './pages/5.Training/Training';
+import Clients from './pages/6.Clients/Clients';
+import About from './pages/7.About/About';
+import Team from './pages/8.Team/Team';
+
+
+
+
+
+
+
 
 function App() {
 
@@ -18,8 +28,11 @@ function App() {
           <Routes>
           <Route exact path='/' element={<Home />} />
           <Route path='/services' element={<Services />} />
-          <Route path='/portfolio' element={<Portfolio />} />
           <Route path='/contact' element={<Contact />} />
+          <Route path='/training' element={<Training />} />
+          <Route path='/clients' element={<Clients />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/team' element={<Team />} />
           </Routes>
 
           <Footer/>
