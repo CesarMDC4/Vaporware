af99fff6069a487e6ea3403180cecaf0df1d1051
Coolguy1079 <Coolguy1079@gmail.com>
2023-11-21 23:09:16 -0800

    About Page Updated
    The text on our About webpage has been formatted to conform to the original text present on the original About webpage.

--- vite/src/pages/7.About/About.jsx
+++ vite/src/pages/7.About/About.jsx
@@ -2,10 +2,50 @@
 
 const About = () => {
   return (
-    <div>
-      <h1>WE ARE...</h1>
-      <p>Cypress Engineering Group (CEG), providing engineering services to companies primarily in California and Arizona. </p>
-    </div>
+    <section id="about">
+      <style>
+        {`
+          .section-header h3 {
+            font-size: 32px;
+            color: #111;
+            text-transform: uppercase;
+            text-align: center;
+            font-weight: 700;
+            position: relative;
+            padding-bottom: 15px;
+          }
+          .section-header p {
+            text-align: center;
+            padding-bottom: 30px;
+            color: #333;
+            margin: 0 0 30px 0;
+          }
+          h1, h2, h3, h4, h5, h6 {
+            font-family: "Exo", sans-serif;
+            font-weight: 400;
+            margin: 0 0 20px 0;
+            padding: 0;
+          }
+          p {
+            padding: 0;
+            margin: 0 0 30px 0;
+          }
+        `}
+      </style>
+      <div className="container">
+        <header className="section-header">
+          <h3>We are...</h3>
+          <p>
+            Cypress Engineering Group (CEG), providing engineering services to companies primarily in California and
+            Arizona. <br/><br/>
+            CEG (formerly KIE Consultants) was founded by Robby Kroeze and Sefa Isik in 2008. <br/><br/>
+            As a diversified engineering group, CEG prides itself in developing close relationships with clients and
+            always being available for projects in a wide spectrum, no matter the size. At CEG, we are constantly
+            working to improve our services and maintain pace with the latest developments in our industry.
+          </p>
+        </header>
+      </div>
+    </section>
   )
 }
 
