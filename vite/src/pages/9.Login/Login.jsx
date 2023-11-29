673898affadc79cc49281695ff9cbae42b25202a
sachal1 <71674805+sachal1@users.noreply.github.com>
2023-11-28 19:37:18 -0800

    Login page made to fit screen properly + gradient

--- vite/src/pages/9.Login/Login.jsx
+++ vite/src/pages/9.Login/Login.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function Login() {
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-1 bg-gradient-to-t from-stone-300 via-zinc-300 to-white flex flex-col justify-center items-center">
+        <div className="sm:w-full sm:max-w-sm px-6 py-12 lg:px-8">
         <img
           className="mx-auto h-10 w-auto"
           src="https://www.cypresseg.com/img/logo/CypressEG.png"
@@ -27,7 +28,7 @@ export default function Login() {
                 type="email"
                 autoComplete="email"
                 required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-548235ff sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring sm:text-sm sm:leading-6"
               />
             </div>
           </div>
@@ -38,7 +39,7 @@ export default function Login() {
                 Password
               </label>
               <div className="text-sm">
-                <a href="#" className="font-semibold text-548235ff hover:text-green-600">
+                <a href="#" className="font-semibold hover:text-green-600">
                   Forgot password?
                 </a>
               </div>
@@ -50,7 +51,7 @@ export default function Login() {
                 type="password"
                 autoComplete="current-password"
                 required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-548235ff sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring sm:text-sm sm:leading-6"
               />
             </div>
           </div>
@@ -58,7 +59,7 @@ export default function Login() {
           <div>
             <button
               type="submit"
-              className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-548235ff"
+              className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
             >
               Sign in
             </button>
@@ -67,10 +68,11 @@ export default function Login() {
 
         <p className="mt-10 text-center text-sm text-zinc-950">
           Not a member?{' '}
-          <a href="#" className="font-extrabold leading-6 text-548235ff text-zinc-950 hover:text-green-500">
+          <a href="#" className="font-extrabold leading-6 text-zinc-950 hover:text-green-500">
             Start a 14 day free trial
           </a>
         </p>
+        </div>
       </div>
     </div>
   );
