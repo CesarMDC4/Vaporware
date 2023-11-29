b4a38800833220bc7c7aecce47b8464c440f5021
Coolguy1079 <Coolguy1079@gmail.com>
2023-11-29 13:16:43 -0800

    Added the Team to the About page
    Included the Team to the About page. Needs reformatting on the format, and reformatting for the code soon.

--- vite/src/pages/7.About/About.jsx
+++ vite/src/pages/7.About/About.jsx
@@ -16,6 +16,100 @@ const About = () => {
         cssEase: 'linear',
     };
 
+    const fadeInUpStyle = {
+        visibility: 'visible',
+        animationName: 'fadeInUp',
+      };
+    
+      const colLGStyle = {
+        WebkitBoxFlex: 0,
+        MsFlex: '0 0 25%',
+        flex: '0 0 25%',
+        maxWidth: '25%',
+      };
+    
+      const colStyle = {
+        position: 'relative',
+        width: '100%',
+        minHeight: '1px',
+        paddingRight: '15px',
+        paddingLeft: '15px',
+      };
+
+      const centeredStyle = {
+        display: 'inline-block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+      };
+
+      const memberHoverStyle = {
+        background: 'rgba(0, 0, 0, 0.7)',
+        opacity: 1,
+        transition: '0.4s',
+      };
+    
+      const memberInfoStyle = {
+        opacity: 0,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        borderRadius: '50%',
+        bottom: 0,
+        top: 0,
+        left: 0,
+        right: 0,
+        transition: '0.2s',
+      };
+    
+      const imgFluidStyle = {
+        maxWidth: '100%',
+        height: 'auto',
+      };
+    
+      const imgStyle = {
+        maxWidth: '100%',
+        height: 'auto',
+        verticalAlign: 'middle',
+        borderStyle: 'none',
+      };
+
+      const img =  {
+        verticalAlign: 'middle',
+        borderStyle: 'none',
+        }
+
+    const linkStyle = {
+        color: '-webkit-link',
+        cursor: 'auto',
+        color: '#007bff',
+            textDecoration: 'none',
+            backgroundColor: 'transparent',
+            WebkitTextDecorationSkip: 'objects',
+        };
+    
+        const faLinkedinStyle = {
+        content: '"\f0e1"',
+        };
+    
+        const faStyle = {
+        display: 'inline-block',
+        font: 'normal normal normal 14px/1 FontAwesome',
+        fontSize: 'inherit',
+        textRendering: 'auto',
+        WebkitFontSmoothing: 'antialiased',
+        MozOsxFontSmoothing: 'grayscale',
+        };
+    
+        const socialIconStyle = {
+        fontSize: '18px',
+        margin: '0 2px',
+        };
+    
+        const socialLinkStyle = {
+        color: '#fff',
+        };
+
     const logos = [
         // Add the URLs of your logos here
         'src/assets/img/clients/American_Cooling.png',
@@ -125,6 +219,341 @@ const About = () => {
                 </Slider>
             </div>  
         </div>
+        <div className="mb-12">
+            <h1 className="text-center font-bold text-4xl mt-4 mb-12 font-sans">THE TEAM</h1>
+            <div className="w-3/4 mx-auto">
+                
+        
+    <section id="team">
+      <style>
+        {`
+          .fadeInUp {
+            animation-name: fadeInUp;
+          }
+          @media (min-width: 992px) {
+            .col-lg-3 {
+              -webkit-box-flex: 0;
+              -ms-flex: 0 0 25%;
+              flex: 0 0 25%;
+              max-width: 25%;
+            }
+          }
+          #team .centered {
+            display: inline-block;
+            margin-left: auto;
+            margin-right: auto;
+          }
+        `}
+      </style>
+
+      <div className="container">
+      <style>
+        {`
+          #team .member:hover .member-info {
+            background: rgba(0, 0, 0, 0.7);
+            opacity: 1;
+            transition: 0.4s;
+          }
+          #team .member .member-info {
+            opacity: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: absolute;
+            border-radius: 50%;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            transition: 0.2s;
+            color: white;
+          }
+          .img-fluid {
+            max-width: 100%;
+            height: auto;
+            margin-left: auto;
+            margin-right: auto;
+            vertical-align: middle;
+            border-style: none;
+          }
+          #team .member {
+            text-align: center;
+            margin-bottom: 20px;
+            background: #fff;
+            position: relative;
+            }
+          #team img {
+            border-radius: 50%;
+          }
+          img {
+            vertical-align: middle;
+            border-style: none;
+            }
+          /* Adding the provided styles for columns */
+          .col, .col-1, .col-10, .col-11, .col-12, .col-2, .col-3, .col-4, .col-5, .col-6, .col-7, .col-8, .col-9, .col-auto, 
+          .col-lg, .col-lg-1, .col-lg-10, .col-lg-11, .col-lg-12, .col-lg-2, .col-lg-3, .col-lg-4, .col-lg-5, .col-lg-6, .col-lg-7, 
+          .col-lg-8, .col-lg-9, .col-lg-auto, .col-md, .col-md-1, .col-md-10, .col-md-11, .col-md-12, .col-md-2, .col-md-3, .col-md-4, 
+          .col-md-5, .col-md-6, .col-md-7, .col-md-8, .col-md-9, .col-md-auto, .col-sm, .col-sm-1, .col-sm-10, .col-sm-11, .col-sm-12, 
+          .col-sm-2, .col-sm-3, .col-sm-4, .col-sm-5, .col-sm-6, .col-sm-7, .col-sm-8, .col-sm-9, .col-sm-auto, .col-xl, .col-xl-1, 
+          .col-xl-10, .col-xl-11, .col-xl-12, .col-xl-2, .col-xl-3, .col-xl-4, .col-xl-5, .col-xl-6, .col-xl-7, .col-xl-8, .col-xl-9, 
+          .col-xl-auto {
+            position: relative;
+            width: 100%;
+            min-height: 1px;
+            padding-right: 15px;
+            padding-left: 15px;
+          }
+        `}
+      </style>
+        <div className="section-header wow fadeInUp" style={fadeInUpStyle}>
+          <h3>Team</h3>
+          <p>Here is the competent Cypress Team. Cypress Engineering Group combines experience and energy of its competent engineers from multi-disciplinary backgrounds and high educational degrees.</p>
+        </div>
+        <div className="row">
+          {/* Individual team members */}
+          {/* You'd replicate this structure for each team member */}
+          <div className="col-lg-3 col-md-4 wow fadeInUp centered" style={{ ...colLGStyle, ...centeredStyle }}>
+            <div className="member">
+              <img src="src/assets/img/sefa.jpg" className="img-fluid" alt="Sefa Isik"/>
+              <div className="member-info">
+                <div className="member-info-content"> 
+                  <h4>Sefa Isik</h4>
+                  <span>Founder, PE, MSME, CSP</span>
+                 <div className="social">
+                    <a href="https://www.linkedin.com/in/sefa-isik-pe-csp-15bb4843/" target="_blank" rel="noopener" style={linkStyle}>
+                        <i className="fa fa-linkedin" style={faStyle}>IN</i>
+                    </a>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* End of individual team member */}
+          {/* Add more team members similarly */}
+          <div className="col-lg-3 col-md-4 wow fadeInUp centered" style={{ ...colLGStyle, ...centeredStyle }}>
+            <div className="member">
+              <img src="src/assets/img/Metin.png" className="img-fluid" alt="Metin Serttunc"/>
+              <div className="member-info">
+                <div className="member-info-content"> 
+                  <h4>Metin Serttunc</h4>
+                  <span>Principal, PE, CBCP, CEM</span>
+                 <div className="social">
+                    <a href="https://www.cypresseg.com" target="_blank" rel="noopener" style={linkStyle}>
+                        <i className="fa fa-linkedin" style={faStyle}>IN</i>
+                    </a>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* End of individual team member */}
+          {/* Add more team members similarly */}
+          <div className="col-lg-3 col-md-4 wow fadeInUp centered" style={{ ...colLGStyle, ...centeredStyle }}>
+            <div className="member">
+              <img src="src/assets/img/scott.png" className="img-fluid" alt="Scott Stroshane"/>
+              <div className="member-info">
+                <div className="member-info-content"> 
+                  <h4>Scott Stroshane</h4>
+                  <span>Senior Project Manager, PE</span>
+                 <div className="social">
+                    <a href="https://www.cypresseg.com" target="_blank" rel="noopener" style={linkStyle}>
+                        <i className="fa fa-linkedin" style={faStyle}>IN</i>
+                    </a>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* End of individual team member */}
+          {/* Add more team members similarly */}
+          <div className="col-lg-3 col-md-4 wow fadeInUp centered" style={{ ...colLGStyle, ...centeredStyle }}>
+            <div className="member">
+              <img src="src/assets/img/Steven.png" className="img-fluid" alt="Steven E Rawson"/>
+              <div className="member-info">
+                <div className="member-info-content"> 
+                  <h4>Steven E Rawson</h4>
+                  <span>Project Manager, PE</span>
+                 <div className="social">
+                    <a href="https://www.linkedin.com/in/steven-e-rawson-p-e-025a659" target="_blank" rel="noopener" style={linkStyle}>
+                        <i className="fa fa-linkedin" style={faStyle}>IN</i>
+                    </a>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* End of individual team member */}
+          {/* Add more team members similarly */}
+          <div className="col-lg-3 col-md-4 wow fadeInUp centered" style={{ ...colLGStyle, ...centeredStyle }}>
+            <div className="member">
+              <img src="src/assets/img/Chahan.png" className="img-fluid" alt="Chahan Shah"/>
+              <div className="member-info">
+                <div className="member-info-content"> 
+                  <h4>Chahan Shah</h4>
+                  <span>Project Manager, PE, CxA</span>
+                 <div className="social">
+                    <a href="https://www.linkedin.com/in/chahan-shah-p-e-cxa-9934268b" target="_blank" rel="noopener" style={linkStyle}>
+                        <i className="fa fa-linkedin" style={faStyle}>IN</i>
+                    </a>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* End of individual team member */}
+          {/* Add more team members similarly */}
+          <div className="col-lg-3 col-md-4 wow fadeInUp centered" style={{ ...colLGStyle, ...centeredStyle }}>
+            <div className="member">
+              <img src="src/assets/img/Rod.jpg" className="img-fluid" alt="Rod Revera"/>
+              <div className="member-info">
+                <div className="member-info-content"> 
+                  <h4>Rod Revera</h4>
+                  <span>Project Manager, PE</span>
+                 <div className="social">
+                    <a href="https://www.cypresseg.com" target="_blank" rel="noopener" style={linkStyle}>
+                        <i className="fa fa-linkedin" style={faStyle}>IN</i>
+                    </a>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* End of individual team member */}
+          {/* Add more team members similarly */}
+          <div className="col-lg-3 col-md-4 wow fadeInUp centered" style={{ ...colLGStyle, ...centeredStyle }}>
+            <div className="member">
+              <img src="src/assets/img/mfatihkonus.jpg" className="img-fluid" alt="Fatih Konus"/>
+              <div className="member-info">
+                <div className="member-info-content"> 
+                  <h4>Fatih Konus</h4>
+                  <span>Project Engineer, PHD, QISP</span>
+                 <div className="social">
+                    <a href="https://www.linkedin.com/in/m-fatih-konus-ph-d-93206072/" target="_blank" rel="noopener" style={linkStyle}>
+                        <i className="fa fa-linkedin" style={faStyle}>IN</i>
+                    </a>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* End of individual team member */}
+          {/* Add more team members similarly */}
+          <div className="col-lg-3 col-md-4 wow fadeInUp centered" style={{ ...colLGStyle, ...centeredStyle }}>
+            <div className="member">
+              <img src="src/assets/img/JulieLin.png" className="img-fluid" alt="Julie Lin"/>
+              <div className="member-info">
+                <div className="member-info-content"> 
+                  <h4>Julie Lin</h4>
+                  <span>Production Manager</span>
+                 <div className="social">
+                    <a href="https://www.linkedin.com/in/julielin2/" target="_blank" rel="noopener" style={linkStyle}>
+                        <i className="fa fa-linkedin" style={faStyle}>IN</i>
+                    </a>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* End of individual team member */}
+          {/* Add more team members similarly */}
+          <div className="col-lg-3 col-md-4 wow fadeInUp centered" style={{ ...colLGStyle, ...centeredStyle }}>
+            <div className="member">
+              <img src="src/assets/img/Hakan.png" className="img-fluid" alt="Hakan Eren"/>
+              <div className="member-info">
+                <div className="member-info-content"> 
+                  <h4>Hakan Eren</h4>
+                  <span>Regulatory Compliance Manager</span>
+                 <div className="social">
+                    <a href="https://tr.linkedin.com/in/hakan-eren-9170bb49" target="_blank" rel="noopener" style={linkStyle}>
+                        <i className="fa fa-linkedin" style={faStyle}>IN</i>
+                    </a>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* End of individual team member */}
+          {/* Add more team members similarly */}
+          <div className="col-lg-3 col-md-4 wow fadeInUp centered" style={{ ...colLGStyle, ...centeredStyle }}>
+            <div className="member">
+              <img src="src/assets/img/Daniel.png" className="img-fluid" alt="Daniel Soto"/>
+              <div className="member-info">
+                <div className="member-info-content"> 
+                  <h4>Daniel Soto</h4>
+                  <span>Designer</span>
+                 <div className="social">
+                    <a href="https://www.linkedin.com/in/daniel-soto-48117947" target="_blank" rel="noopener" style={linkStyle}>
+                        <i className="fa fa-linkedin" style={faStyle}>IN</i>
+                    </a>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* End of individual team member */}
+          {/* Add more team members similarly */}
+          <div className="col-lg-3 col-md-4 wow fadeInUp centered" style={{ ...colLGStyle, ...centeredStyle }}>
+            <div className="member">
+              <img src="src/assets/img/placeholder_female.png" className="img-fluid" alt="Meselina Garcia"/>
+              <div className="member-info">
+                <div className="member-info-content"> 
+                  <h4>Meselina Garcia</h4>
+                  <span></span>
+                 <div className="social">
+                    <a href="https://www.cypresseg.com" target="_blank" rel="noopener" style={linkStyle}>
+                        <i className="fa fa-linkedin" style={faStyle}>IN</i>
+                    </a>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* End of individual team member */}
+          {/* Add more team members similarly */}
+          <div className="col-lg-3 col-md-4 wow fadeInUp centered" style={{ ...colLGStyle, ...centeredStyle }}>
+            <div className="member">
+              <img src="src/assets/img/placeholder_female.png" className="img-fluid" alt="Kathryn Kroeze"/>
+              <div className="member-info">
+                <div className="member-info-content"> 
+                  <h4>Kathryn Kroeze</h4>
+                  <span>Bookkeeper</span>
+                 <div className="social">
+                    <a href="https://www.cypresseg.com" target="_blank" rel="noopener" style={linkStyle}>
+                        <i className="fa fa-linkedin" style={faStyle}>IN</i>
+                    </a>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* End of individual team member */}
+          {/* Add more team members similarly */}
+          <div className="col-lg-3 col-md-4 wow fadeInUp centered" style={{ ...colLGStyle, ...centeredStyle }}>
+            <div className="member">
+              <img src="src/assets/img/RobbyKroeze.png" className="img-fluid" alt="Robby Kroeze"/>
+              <div className="member-info">
+                <div className="member-info-content"> 
+                  <h4>Robby Kroeze</h4>
+                  <span>Retired</span>
+                 <div className="social">
+                    <a href="https://www.cypresseg.com" target="_blank" rel="noopener" style={linkStyle}>
+                        <i className="fa fa-linkedin" style={faStyle}>IN</i>
+                    </a>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* End of individual team member */}
+          {/* Add more team members similarly */}
+        </div>
+      </div>
+    </section>
+    </div>  
+        </div>
+
     </section>
     )
 }
