84208cd871aae6db22c7fb19c8029e48839bc8f1
leahgranado <leahgranado@csus.edu>
2023-11-30 08:31:12 -0800

    Service Card Tweaks
    - Space between register button & cards
    - Edited font weight
    - Changed font color to black

--- vite/src/pages/1.home/Home.jsx
+++ vite/src/pages/1.home/Home.jsx
@@ -11,19 +11,23 @@ const Home = () => {
           <p class="pb-5">Click below to go to our new training registration page for more info and to register.</p>
           <button class="bg-green-900 text-white hover:bg-white hover:text-green-900 font-bold py-2 px-4 rounded-full mb-5">Register Now!</button>
 
+          {/* Space between header and service cards */}
+          <div class="my-24"></div>
+
           {/* Service Cards */}
-          <div class ="max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-0">
+          <div class="max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-0">
 
             {/* Service card 1: HVAC, Plumbing, Fire Protection, Building Cx, Energy Audits  */}
             <div class= "max-w-sm bg-transparent">
               <div class="p-5 bg-transparent">
                 <div class="flex items-center">
                   <ion-icon name="cog-outline"></ion-icon>
+                  <div class="p-10"></div>
                 </div>
                 <a href="#">
-                  <s1 class="mb-4 text-lg font-bold tracking-tight text-black text-center">HVAC, Plumbing, Fire Protection, Building Cx, Energy Audits</s1>
+                  <s1 class="mb-4 text-lg tracking-tight text-black text-center">HVAC, Plumbing, Fire Protection, Building Cx, Energy Audits</s1>
                 </a>
-                <p class="mt-4 list-disc pl-5 text-gray-500 text-left">Our field experience combined with the knowledge of engineering concepts gives us an advantageous perspective that allows us to design better systems with constructability and practicality in mind as well as conduct and prepare constructability review comments for clients.</p>
+                <p class="mt-4 list-disc pl-5 text-black text-left">Our field experience combined with the knowledge of engineering concepts gives us an advantageous perspective that allows us to design better systems with constructability and practicality in mind as well as conduct and prepare constructability review comments for clients.</p>
               </div>
             </div>
 
@@ -32,11 +36,12 @@ const Home = () => {
               <div class="p-5 bg-transparent">
                 <div class="flex items-center">
                   <ion-icon name="snow-outline"></ion-icon>
+                  <div class="p-10"></div>
                 </div>
                 <a href="#">
-                  <s2 class="mb-4 text-lg font-bold font-['Open Sans'] tracking-tight text-black text-center">Industrial Refrigeration</s2>
+                  <s2 class="mb-4 text-lg tracking-tight text-black text-center">Industrial Refrigeration</s2>
                 </a>
-                <p class="mt-4 list-disc pl-5 text-gray-500 text-left">Our promise to you is to provide you the latest technology, latest trends, and up-to-date refrigeration solutions.</p>
+                <p class="mt-4 list-disc pl-5 text-black text-left">Our promise to you is to provide you the latest technology, latest trends, and up-to-date refrigeration solutions.</p>
               </div>
             </div>
 
@@ -46,11 +51,12 @@ const Home = () => {
               <div class="p-5 bg-transparent">
                 <div class="flex items-center">
                   <ion-icon name="leaf"></ion-icon>
+                  <div class="p-10"></div>
                 </div>
                 <a href="#">
-                  <s2 class="mb-4 text-lg font-bold tracking-tight text-black text-center">Environmental & Regulatory Compliance</s2>
+                  <s2 class="mb-4 text-lg tracking-tight text-black text-center">Environmental & Regulatory Compliance</s2>
                 </a>
-                <p class="mt-4 list-disc pl-5 text-gray-500 text-left">We love ammonia, and we love it safe. We will make sure that you remain in safe operating limits.</p>
+                <p class="mt-4 list-disc pl-5 text-black text-left">We love ammonia, and we love it safe. We will make sure that you remain in safe operating limits.</p>
               </div>
             </div>
 
@@ -59,24 +65,26 @@ const Home = () => {
               <div class="p-5 bg-transparent">
                 <div class="flex items-center">
                   <ion-icon name="school"></ion-icon>
+                  <div class="p-10"></div>
                 </div>
                 <a href="#">
-                  <s2 class="mb-4 text-lg font-bold tracking-tight text-black text-center">Training</s2>
+                  <s2 class="mb-4 text-lg tracking-tight text-black text-center">Training</s2>
                 </a>
-                <p class="mt-4 list-disc pl-5 text-gray-500 text-left">We provide RMP/PSM regulatory compliance, ammonia refrigeration, HAZWOPER, and industrial general permit storm water compliance training.</p>
+                <p class="mt-4 list-disc pl-5 text-black text-left">We provide RMP/PSM regulatory compliance, ammonia refrigeration, HAZWOPER, and industrial general permit storm water compliance training.</p>
               </div>
             </div>
 
             {/* Service card 5: Cypress Engineering Group  */}
             <div class= "max-w-sm bg-transparent">
-              <a href = "#">
-                <img class ="rounded-t-lg" src ="" alt=""/>
-              </a>
               <div class="p-5 bg-transparent">
+                <div class="flex items-center">
+                  
+                  <div class="p-10"></div>
+                </div>
                 <a href="#">
-                  <s2 class="mb-4 text-lg font-bold tracking-tight text-black text-center">Cypress Engineering Group</s2>
+                  <s2 class="mb-4 text-lg tracking-tight text-black text-center">Cypress Engineering Group</s2>
                 </a>
-                <p class="mt-4 list-disc pl-5 text-gray-500 text-left">KIE Consultants has transformed into Cypress Engineering Group with all good things about KIE remaining the same, plus new team members and refreshed enthusiasm.</p>
+                <p class="mt-4 list-disc pl-5 text-black text-left">KIE Consultants has transformed into Cypress Engineering Group with all good things about KIE remaining the same, plus new team members and refreshed enthusiasm.</p>
               </div>
             </div>
           </div>
