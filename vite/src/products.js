a7a4347b50c105cd68b4320798c9d8565085668d
Cesar Martin Del Campo <cmdc1999@gmail.com>
2024-02-09 18:40:00 -0800

    Product Item object and the hard coded info

---
+++ vite/src/products.js
@@ -0,0 +1,36 @@
+import training1 from "./assets/img/regulatory_compliance.png";
+import training2 from "./assets/img/refrigeration.png";
+import training3 from "./assets/img/industrial_refrigeration.png";
+import training4 from "./assets/img/Hazwoper.png";
+export const Products = [
+    {
+        id: "CEG-TR-101",
+        productName: "CalARP/RMP/PSM/CERS/Tier 2 Regulatory Compliance",
+        price: 500.0,
+        productImage:training1,
+        productDescription: "description",
+    },
+    {
+        id: "CEG-TR-201",
+        productName: "Basic Industrial Ammonia Refrigeration (RETA IR Book 1)",
+        price: 500.0,
+        productImage:training2,
+        productDescription: "description",
+    },
+    {
+        id: "CEG-TR-202",
+        productName: "Intermediate Industrial Ammonia Refrigeration (RETA IR Book 2)",
+        price: 500.0,
+        productImage:training3,
+        productDescription: "description",
+    },
+    {
+        id: "CEG-TR-301",
+        productName: "HAZWOPER 8-hour Refresher Training",
+        price: 500.0,
+        productImage:training4,
+        productDescription: "description",
+    },
+
+
+]
