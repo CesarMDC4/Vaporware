6fe4ba547c167c0ef5b2f1dffb2cca79242702f9
BryantG <Bgalo89@gmail.com>
2024-02-11 11:53:57 -0800

    show google form registration when user clicks register on nav bar

--- vite/src/pages/5.Training/Register.jsx
+++ vite/src/pages/5.Training/Register.jsx
@@ -1,7 +1,20 @@
+import React from 'react';
+
 const Register = () => {
-    return (
-      <div>Register</div>
-    )
-  }
-  
-  export default Register
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+
+      {/* Embedded Google Doc form link to Cypress Engineering Registration form */}
+      <iframe
+        title="Google Doc Form"
+        src="https://docs.google.com/forms/d/e/1FAIpQLSczcAzQacJndpHIUeg5i5oFpJT-QF4zPUgr2zGlK_9OdrFQGw/viewform?embedded=true"
+        width="640"
+        height="800"
+      >
+        Loading...
+      </iframe>
+    </div>
+  );
+};
+
+export default Register;
