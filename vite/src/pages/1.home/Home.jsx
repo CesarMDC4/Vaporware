756b7602cdd467190444cd305e4dbb66dfef7e0c
ElesiaThan <aiselenaht@gmail.com>
2024-02-21 18:46:14 -0800

    VAP-71

--- vite/src/pages/1.home/Home.jsx
+++ vite/src/pages/1.home/Home.jsx
@@ -21,8 +21,8 @@ const Home = () => {
             <div class= "max-w-sm bg-transparent">
               <ion-icon name="cog-outline"></ion-icon>
               <div class="p-5 bg-transparent">
-                <a href="#">
-                  <s2 class="mb-4 text-lg tracking-tight text-black text-center">HVAC, Plumbing, Fire Protection, Building Cx, Energy Audits</s2>
+                <a href='/services/service1'>
+                  <s2 class="mb-4 text-lg tracking-tight text-black text-center hover:text-blue-600">HVAC, Plumbing, Fire Protection, Building Cx, Energy Audits</s2>
                 </a>
                 <p class="mt-4 list-disc pl-5 text-black text-left">Our field experience combined with the knowledge of engineering concepts gives us an advantageous perspective that allows us to design better systems with constructability and practicality in mind as well as conduct and prepare constructability review comments for clients.</p>
               </div>
@@ -32,8 +32,8 @@ const Home = () => {
             <div class= "max-w-sm bg-transparent">
               <ion-icon name="snow-outline"></ion-icon>
               <div class="p-5 bg-transparent">
-                <a href="#">
-                  <s2 class="mb-4 text-lg tracking-tight text-black text-center">Industrial Refrigeration</s2>
+                <a href='/services/service3'>
+                  <s2 class="mb-4 text-lg tracking-tight text-black text-center hover:text-blue-600">Industrial Refrigeration</s2>
                 </a>
                 <p class="mt-4 list-disc pl-5 text-black text-left">Our promise to you is to provide you the latest technology, latest trends, and up-to-date refrigeration solutions.</p>
               </div>
@@ -43,8 +43,8 @@ const Home = () => {
             <div class= "max-w-sm bg-transparent">
               <div class="p-5 bg-transparent">
                 <div class="p-7"></div>
-                <a href="#">
-                  <s2 class="mb-4 text-lg tracking-tight text-black text-center">Cypress Engineering Group</s2>
+                <a href='/about'>
+                  <s2 class="mb-4 text-lg tracking-tight text-black text-center hover:text-blue-600">Cypress Engineering Group</s2>
                 </a>
                 <p class="mt-4 list-disc pl-5 text-black text-left">KIE Consultants has transformed into Cypress Engineering Group with all good things about KIE remaining the same, plus new team members and refreshed enthusiasm.</p>
               </div>
@@ -53,10 +53,10 @@ const Home = () => {
             {/* Service card 4: Environmental & Regulatory Compliance  */}
             <div class= "max-w-sm bg-transparent">
               <ion-icon name="leaf"></ion-icon>
-              <a href = "#"></a>
+              
               <div class="p-5 bg-transparent">
-                <a href="#">
-                  <s2 class="mb-4 text-lg tracking-tight text-black text-center">Environmental & Regulatory Compliance</s2>
+                <a href='/services/service4'>
+                  <s2 class="mb-4 text-lg tracking-tight text-black text-center hover:text-blue-600">Environmental & Regulatory Compliance</s2>
                 </a>
                 <p class="mt-4 list-disc pl-5 text-black text-left">We love ammonia, and we love it safe. We will make sure that you remain in safe operating limits.</p>
               </div>
@@ -66,8 +66,8 @@ const Home = () => {
             <div class= "max-w-sm bg-transparent">
               <ion-icon name="school"></ion-icon>
               <div class="p-5 bg-transparent">
-                <a href="#">
-                  <s2 class="mb-4 text-lg tracking-tight text-black text-center">Training</s2>
+                <a href='/services/service6'>
+                  <s2 class="mb-4 text-lg tracking-tight text-black text-center hover:text-blue-600">Training</s2>
                 </a>
                 <p class="mt-4 list-disc pl-5 text-black text-left">We provide RMP/PSM regulatory compliance, ammonia refrigeration, HAZWOPER, and industrial general permit storm water compliance training.</p>
               </div>
