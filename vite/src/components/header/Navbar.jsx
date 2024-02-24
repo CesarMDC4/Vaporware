fdf415f519befee20faa69ad147c658e7b8e028b
CesarMDC4 <cesarmartindelcampo@csus.edu>
2024-02-24 09:06:22 -0800

    Deleted Profile pic on navbar VAP-74

--- vite/src/components/header/Navbar.jsx
+++ vite/src/components/header/Navbar.jsx
@@ -47,23 +47,6 @@ export default function Example() {
                 </Disclosure.Button>
               </div>
 
-              {/* Right section for user profile*/}
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button className="flex text-sm rounded-full focus:outline-none">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt="User"
-                      />
-                    </Menu.Button>
-                  </div>
-                  {/* ... Menu Items ... */}
-                </Menu>
-              </div>
 
               {/* Right section for navigation links */}
               <div className="hidden sm:block sm:ml-6">
