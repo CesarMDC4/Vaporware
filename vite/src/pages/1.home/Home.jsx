7b83e6738d221b18c3f50f2220cb09d4b4ff7ae5
Cesar Martin Del Campo <cmdc1999@gmail.com>
2024-02-25 19:15:38 -0800

    Removing logo between register button and cards

--- vite/src/pages/1.home/Home.jsx
+++ vite/src/pages/1.home/Home.jsx
@@ -11,9 +11,6 @@ const Home = () => {
           <p class="pb-5">Click below to go to our new training registration page for more info and to register.</p>
           <button class="bg-green-900 text-white hover:bg-white hover:text-green-900 font-bold py-2 px-4 rounded-full mb-5">Register Now!</button>
 
-          <div className="flex justify-center items-center mb-4"> 
-            <img src="/src/assets/img/logo/CypressEG.png" alt="Cypress Engineering Group Logo" className="h-20 w-auto" /> 
-          </div>
 
           {/* Space between header and service cards */}
           <div class="my-10"></div>
